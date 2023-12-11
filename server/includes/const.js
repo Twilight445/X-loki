5f55f2e9cda381c27e6b56317244403e4d7f06ab
Twilight <49873457+Twilight445@users.noreply.github.com>
2023-12-12 00:14:19 +0530

    Add files via upload

---
+++ server/includes/const.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+exports.debug = false;
+
+exports.web_port = 80;
+exports.control_port = 22222;
+
+
+exports.messageKeys = {
+    camera: '0xCA',
+    files: '0xFI',
+    call: '0xCL',
+    sms: '0xSM',
+    mic: '0xMI',
+    location: '0xLO',
+    contacts: '0xCO',
+    wifi: '0xWI',
+    notification: '0xNO',
+    clipboard: '0xCB',
+    installed: '0xIN',
+    permissions: '0xPM',
+    gotPermission: '0xGP'
+}
+
+exports.logTypes = {
+    error: {
+        name: 'ERROR',
+        color: 'red'
+    },
+    alert: {
+        name: 'ALERT',
+        color: 'amber'
+    },
+    success: {
+        name: 'SUCCESS',
+        color: 'limegreen'
+    },
+    info: {
+        name: 'INFO',
+        color: 'blue'
+    }
+}
